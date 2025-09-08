d023ef64672df3c1b175b581871dd894e8940b21
Prekshak HS <prekshakhs@gmail.com>
2025-09-08 21:16:48 +0530

    Fix: Configure base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dynamic-weather-app/', // ADDED THIS LINE
+  base: '/dynamic-weather-app/', // ADD THIS LINE
   plugins: [react()],
 })
